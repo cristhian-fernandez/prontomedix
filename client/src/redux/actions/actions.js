b391289b2b9a1cbebff4b4ec5f5e72a419d5f861
Cristhian Fernandez <cjfernandez29@gmail.com>
2022-08-13 01:54:26 -0500

    update urlTest actions

--- client/src/redux/actions/actions.js
+++ client/src/redux/actions/actions.js
@@ -13,8 +13,8 @@ import {
 
 import axios from "axios";
 
-const urlTest = 'http://localhost:3001/api/tests';
-// const urlTest = 'https://bdprontomedix.herokuapp.com/api/tests';
+// const urlTest = 'http://localhost:3001/api/tests';
+const urlTest = 'https://bdprontomedix.herokuapp.com/api/tests';
 
 console.log('urlTest',urlTest);
 export const getAllTests = () => {
